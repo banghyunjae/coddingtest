f0750017182849d5518d1d844458e7cb14ca8f38
banghyunjae <127192957+banghyunjae@users.noreply.github.com>
2023-04-26 09:40:33 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,10 @@
+
+def solution(date1, date2):
+    for i, j in zip(date1, date2):
+        if i < j:
+            return 1
+        elif i > j:
+            return 0
+        else:
+            continue
+    return 0
