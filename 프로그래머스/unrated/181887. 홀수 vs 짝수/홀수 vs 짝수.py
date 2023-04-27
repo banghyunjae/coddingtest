a0997f525a5566d099dd772467cadf4b968110ee
banghyunjae <127192957+banghyunjae@users.noreply.github.com>
2023-04-27 09:52:05 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,16 @@
+def solution(num_list):
+    odd_sum = 0
+    even_sum = 0
+    
+    for i in range(len(num_list)):
+        if i % 2 == 0:
+            even_sum += num_list[i]
+        else:
+            odd_sum += num_list[i]
+    
+    if odd_sum > even_sum:
+        return odd_sum
+    elif odd_sum < even_sum:
+        return even_sum
+    else:
+        return odd_sum 
